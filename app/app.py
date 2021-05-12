c64e79c0c2a20439fc52051117b1b4b14d718a9e
1edv <edv@mit.edu>
2021-05-12 18:08:41 +0000

    Image border added

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 
-####
 import sys, re ,SessionState
 sys.path.insert(0, './')
 import aux_app,app_aux
@@ -244,8 +243,8 @@ st.set_page_config(
  
 #st.write('Path Prefix is ' + path_prefix)
 
-if 1 : #"platform" in os.environ:
-    if  1 : # os.environ['platform'] == 'streamlit_sharing' :
+if "platform" in os.environ:
+    if  os.environ['platform'] == 'streamlit_sharing' :
         with st.beta_container() : 
             st.write("""
             [![Paper DOI : https://doi.org/10.1101/2021.02.17.430503](https://img.shields.io/badge/DOI-10.1101%2F2021.02.17.430503-blue)](https://doi.org/10.1101/2021.02.17.430503)&nbsp[![Star](https://img.shields.io/github/stars/1edv/evolution.svg?logo=github&style=social)](https://github.com/1edv/evolution)
@@ -256,14 +255,15 @@ if 1 : #"platform" in os.environ:
             st.title('')
             st.header('Click on the image below to use the live app now 👇')
             st.header('')
-            st.write(""" 
-            | [![App URL](https://raw.githubusercontent.com/1edv/evolution/master/app/overview.png)](https://evolution-app-vbxxkl6a7a-ue.a.run.app/) |
-            | ------ |
-            """)
             if 0 : 
-                st.markdown('''
+                st.write(""" 
+                | [![App URL](https://raw.githubusercontent.com/1edv/evolution/master/app/overview.png)](https://evolution-app-vbxxkl6a7a-ue.a.run.app/) |
+                | ------ |
+                """)
+            if 1 : 
+                st.write('''
                     <p align = 'center'>
-                    <a href='https://evolution-app-vbxxkl6a7a-ue.a.run.app/'><img width = "100%" style="border:5px solid black" src="https://raw.githubusercontent.com/1edv/evolution/master/app/overview.png"/></a>  
+                    <a href='https://evolution-app-vbxxkl6a7a-ue.a.run.app/'><img width = "75%" style="border:4px solid black" src="https://raw.githubusercontent.com/1edv/evolution/master/app/overview.png"/></a>  
 
                     </p>''',
                     unsafe_allow_html=True
