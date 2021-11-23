8275a330224014ed811e4a4ea8690b832ba7b632
1edv <edv@mit.edu>
2021-11-23 14:04:58 +0000

    Updated the test file

--- app/app.py
+++ app/app.py
@@ -495,7 +495,7 @@ with st.container() :
         st.write('* You can download a sample input file below.')
         
         ### START : This block of code should be ignored by users to avoid confusion 
-        df = pd.read_csv(path_prefix+'sample_seqs_vs_gluexp.txt', sep = '\t' , header = None)
+        df = pd.read_csv(path_prefix+'Random_testdata_complex_media.txt', sep = '\t' , header = None)
         sample_list = population_remove_flank(list(df.iloc[:,0].values)) # Removing the flanks is necessary here (see Methods section for details.)
         tmp_download_link = download_link("\n".join(sample_list), 'example_input_file.txt', 'Click here to download an example sequence input file')
         st.markdown(tmp_download_link, unsafe_allow_html=True)
@@ -654,7 +654,7 @@ if valid_input :
             plt.plot(x,y, c='k' ,label='Expression Distribution')
             plt.fill_between(x, 0, y ,color='gray' ,alpha=0.5)
             st.pyplot(fig)
-            with open(path_prefix+'sample_seqs_vs_gluexp.txt') as f:
+            with open(path_prefix+'Random_testdata_complex_media.txt') as f:
                 reader = csv.reader(f, delimiter="\t")
                 d = list(reader)
 
